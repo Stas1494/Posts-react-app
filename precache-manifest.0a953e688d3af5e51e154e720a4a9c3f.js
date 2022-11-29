a0ae15e8124c8849c6c7ad5f6984d17e432a5310
Stas1494 <stason.1494@yandex.ru>
2022-11-29 15:49:45 +0500

    Updates

---
+++ precache-manifest.0a953e688d3af5e51e154e720a4a9c3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06313e244d00735f18bf",
+    "url": "/Posts-react-app/static/css/main.2bc41259.chunk.css"
+  },
+  {
+    "revision": "06313e244d00735f18bf",
+    "url": "/Posts-react-app/static/js/main.06313e24.chunk.js"
+  },
+  {
+    "revision": "82817f0284e379aa808f",
+    "url": "/Posts-react-app/static/js/1.82817f02.chunk.js"
+  },
+  {
+    "revision": "b73fb47ecd91a28ee483",
+    "url": "/Posts-react-app/static/js/runtime~main.b73fb47e.js"
+  },
+  {
+    "revision": "adacaba5a7db0aa68860f077ff27c8ca",
+    "url": "/Posts-react-app/index.html"
+  }
+];
